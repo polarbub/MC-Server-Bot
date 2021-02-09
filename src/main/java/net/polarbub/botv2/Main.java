05b6fd873af9d06dbef6480c02b5295f565d1ddb
Polarbub <71790726+Polarbub@users.noreply.github.com>
2021-02-09 13:35:29 -0600

    Finished gradle config and started on learning api.

--- src/main/java/net/polarbub/botv2/Main.java
+++ src/main/java/net/polarbub/botv2/Main.java
@@ -1,7 +1,30 @@
 package net.polarbub.botv2;
 
-public class Main {
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import javax.security.auth.login.LoginException;
+
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) throws LoginException {
+        JDABuilder.createLight("Nzk2NDYyNTExMjkzMDcxMzYw.X_YRhA.bvzOx0Ls-d481vwnF7tY6RVQzBI", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES).addEventListeners(new Main()).build();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equals("!print"))
+        {
+            System.out.println("hi");
+        }
+    }
+}
+/*public class Main {
     public static void main(String[] args) {
         System.out.println("hi");
     }
-}
+}*/
