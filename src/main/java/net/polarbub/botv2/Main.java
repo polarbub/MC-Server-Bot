3bf1340a13a4a9fb3daed0e240f29f4dffbb2787
polarbub <bryn.hakl@gmail.com>
2021-02-10 11:55:08 -0600

    more build.gradle

--- src/main/java/net/polarbub/botv2/Main.java
+++ src/main/java/net/polarbub/botv2/Main.java
@@ -1,30 +1,46 @@
 package net.polarbub.botv2;
 
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import javax.security.auth.login.LoginException;
+import java.util.Scanner;
 
 public class Main extends ListenerAdapter {
+    public static String pre = ".";
+    public static String tosay = "start";
+    public static MessageChannel sendhere;
+
     public static void main(String[] args) throws LoginException {
-        JDABuilder.createLight("Nzk2NDYyNTExMjkzMDcxMzYw.X_YRhA.bvzOx0Ls-d481vwnF7tY6RVQzBI", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES).addEventListeners(new Main()).build();
+        JDABuilder.createLight("Nzk2NDYyNTExMjkzMDcxMzYw.X_YRhA.t-qv7jVrQ2lauFkKok-tMylECJ8", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES).addEventListeners(new Main()).build();
+        while(true) {
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            tosay = myObj.nextLine();
+            sendhere.sendMessageFormat(tosay).queue();
+        }
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         Message msg = event.getMessage();
-        if (msg.getContentRaw().equals("!print"))
+        MessageChannel channel = event.getChannel();
+        if (msg.getContentRaw().equals(pre + "hi"))
         {
             System.out.println("hi");
+            channel.sendMessageFormat("hi").queue();
+        } else if (msg.getContentRaw().equals(pre + "stopbot")) {
+            System.exit(0);
+        } else {
+            if(tosay.equals(msg.getContentRaw())){
+                int a = 0;
+            } else {
+                System.out.println("Author: " + msg.getAuthor() + " Server: " + event.getGuild() + " Channel: " + msg.getChannel());
+                System.out.println("Content: " + msg.getContentRaw());
+            }
         }
+        sendhere = channel;
     }
 }
-/*public class Main {
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}*/
